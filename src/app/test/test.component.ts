97b6eaf62d7dfaa44a5ce60808242bb33a438f80
Habeeb Rahman <Habeebrahman.MohamedIsmail@agilysys.com>
2020-02-14 17:51:06 +0530

    additional changes.

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -8,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  public myId = "InputId"
+  isDisabled = false;
+ public inputadded = "Initial Text"
+  te = "textError"
+  hasError = true;
+  displayDiv = true;
+  showName=true;
+  color="blue";
   constructor() { }
 
   ngOnInit() {
+  }
+
+  buttonClick(){
+    console.log("Test input added");
+    this.inputadded = "Test input added";
   }
 
   title = "Test component Title"
